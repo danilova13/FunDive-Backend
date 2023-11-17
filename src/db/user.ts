6d75dc117f8e19bc722cbb7c250fc43b5292847e
Anna Danilova <anyadanilova13@gmail.com>
2023-11-17 13:39:58 -0500

    remove extra if check

--- src/db/user.ts
+++ src/db/user.ts
@@ -30,9 +30,7 @@ export class UserDB {
     async getUserById(id: number): Promise<User | null>{
         try {
             const result = await this.pool.query('SELECT * FROM users WHERE id=$1', [id]);
-            if (result.rows[0].length === 0) {
-                return null;
-            }
+          
             if (!result.rows[0]) {
                 return null;
             }
