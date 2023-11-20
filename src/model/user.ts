58b2a6b13cdd7b7add008b5fb5ccb7da3cecd2c2
Anna Danilova <anyadanilova13@gmail.com>
2023-11-20 11:33:22 -0500

    add validation method to user.ts model

--- src/model/user.ts
+++ src/model/user.ts
@@ -1,3 +1,6 @@
+import * as EmailValidator from 'email-validator';
+import { isValidPhoneNumber } from 'libphonenumber-js/min';
+
 export type User = {
     email: string;
     firstName: string;
@@ -11,4 +14,24 @@ export type UserForm = {
     firstName: string;
     lastName: string;
     phone: string;
+}
+
+export async function validateUserForm(userForm: UserForm): Promise<UserForm | null> {
+        // validate email 
+        if(!EmailValidator.validate(userForm.email)){
+            throw new Error('This email is not valid, please enter a valid email!');
+        }
+        // validate phone number 
+        if(!isValidPhoneNumber(userForm.phone)){
+            throw new Error('This phone number is not valid, please enter a valid number!');
+        }
+        // validate first name 
+        if(userForm.firstName === ''){
+            throw new Error('The first name is not valid, please enter a valid first name!')
+        }
+        // validate last name 
+        if(userForm.lastName === ''){
+            throw new Error('The last name is not valid, please enter a valid last name!')
+        } 
+        return userForm;
 }
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,7 +1,6 @@
 import { UserDB } from "../db/user";
 import { User, UserForm } from "../model/user";
-import * as EmailValidator from 'email-validator';
-import { isValidPhoneNumber } from 'libphonenumber-js/min';
+import { validateUserForm } from "../model/user";
 
 export class UserService {
     userDB: UserDB;
@@ -12,24 +11,8 @@ export class UserService {
 
     async createUser(userForm: UserForm): Promise<User | null> {
         try{
-            // validate email 
-            if(!EmailValidator.validate(userForm.email)){
-                throw new Error('This email is not valid, please enter a valid email!');
-            }
-
-            // validate phone number 
-            if(!isValidPhoneNumber(userForm.phone)){
-                throw new Error('This phone number is not valid, please enter a valid number!');
-            }
-
-            // validate first name 
-            if(userForm.firstName === ''){
-                throw new Error('The first name is not valid, please enter a valid first name!')
-            }
-            // validtae last name 
-            if(userForm.lastName === ''){
-                throw new Error('The last name is not valid, please enter a valid last name!')
-            }
+            // validate userForm
+            validateUserForm(userForm);
 
             // build user
             const user: User = {
